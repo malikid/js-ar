9ec8ce68bc6ed653b382c321b4ade81caf3560a2
malikid <malikid.chang@gmail.com>
2016-01-29 01:20:42 +0800

    Added Parse API.

---
+++ js/cara.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function queryParseAPI(classes, where) {
+  return $.ajax({
+    url: "https://api.parse.com/1/classes/" + classes,
+    dataType: 'json',
+    contentType: 'application/json',
+    beforeSend: function(request) {
+      request.setRequestHeader("X-Parse-Application-Id", '7OIk98BkPkgNcZWRIlzaRkz3VPVBjDzkYJ4UGg6p');
+      request.setRequestHeader("X-Parse-REST-API-Key", 'GI4UKLbxWB6blOjORDDl9ukV6Nt53nHfCMloqgmP');
+    },
+    data: "where=" + JSON.stringify(where)
+  });
+}
+
+function getHatStepData() {
+  // return $.ajax({
+  //   url: "https://api.parse.com/1/classes/Checkin",
+  //   dataType: 'json',
+  //   contentType: 'application/json',
+  //   beforeSend: function(request) {
+  //     request.setRequestHeader("X-Parse-Application-Id", '7OIk98BkPkgNcZWRIlzaRkz3VPVBjDzkYJ4UGg6p');
+  //     request.setRequestHeader("X-Parse-REST-API-Key", 'GI4UKLbxWB6blOjORDDl9ukV6Nt53nHfCMloqgmP');
+  //   },
+  //   data: "where=" + JSON.stringify({
+  //     stepEvent: "hat"
+  //   })
+  // });
+  return queryParseAPI("Checkin", {stepEvent: "hat"});
+}
+
+
